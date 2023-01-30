83b86296b8c3de98efe3aac0f0b421a378b23553
Clare Budds <clarebudds@yahoo.co.uk>
2023-01-30 11:46:44 +0000

    Updated js code to target elements in JSON data

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 fetch('https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban')
+
+
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+     
+    const title = data.title;
+    const date = data.date;
+    const shortDescription = data.shortDescription;
+    const creatives = data.creatives;
+
+    const dataContainer = document.getElementById("data-container");
+    dataContainer.innerHTML = `
+        <p>Title: ${title}</p>
+        <p>Date: ${date}</p>
+        <p>Description: ${shortDescription}</p>
+    `;
+    })
+    .catch(error => {
+    console.error("Error fetching data:", error);
+    });
