48893960e9a44422041a6357c1721e003f609d45
Clare Budds <clarebudds@yahoo.co.uk>
2023-01-30 10:34:17 +0000

    Added catch function to log any errors

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 fetch('https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
